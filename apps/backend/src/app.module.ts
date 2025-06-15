7f21a96c7df21aedf7f7307df194a37328579bba
Hanif Ulunnuha Hidayat <ulunnuha.h@gmail.com>
2025-06-15 08:50:24 +0700

    feat: add member module, service, and controller

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FamiliesModule } from './families/families.module';
+import { MembersModule } from './members/members.module';
 
 @Module({
-  imports: [FamiliesModule],
+  imports: [FamiliesModule, MembersModule],
   controllers: [AppController],
   providers: [AppService],
 })
