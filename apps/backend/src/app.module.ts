495da12151a5619bc07190a663aea5c5d0cac15e
Hanif Ulunnuha Hidayat <ulunnuha.h@gmail.com>
2025-06-15 09:15:18 +0700

    feat: setup typeOrm for db connection

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FamiliesModule } from './families/families.module';
 import { MembersModule } from './members/members.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+require('dotenv').config();
 
 @Module({
-  imports: [FamiliesModule, MembersModule],
+  imports: [TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'family-tree',
+      host: process.env["DB_HOST"],
+      port: Number(process.env["DB_PORT"]) || 5432,
+      username: process.env["DB_USERNAME"],
+      password: process.env["DB_PASSWORD"],
+      autoLoadEntities: true,
+      synchronize: true,
+    }), FamiliesModule, MembersModule],
   controllers: [AppController],
   providers: [AppService],
 })
