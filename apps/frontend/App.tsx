4c1abd63cc2af9b6f8a57c5b8293cb72032273b5
Ahmad Naufal <ahmedopank@gmail.com>
2025-06-15 15:26:02 +0700

    feat(gluestack-nativewind): setup gluestack UI and nativewind

--- apps/frontend/App.tsx
+++ apps/frontend/App.tsx
@@ -1,13 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import '@/global.css'
+import { GluestackUIProvider } from './components/ui/gluestack-ui-provider'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <GluestackUIProvider mode="light">
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </GluestackUIProvider>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +21,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,82 @@
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import * as path from "path";
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+import pluginUnusedImports from 'eslint-plugin-unused-imports'
+import pluginImport from 'eslint-plugin-import'
+import pluginReact from 'eslint-plugin-react'
+import pluginReactNative from 'eslint-plugin-react-native'
+import pluginNode from 'eslint-plugin-node'
+import pluginJsdoc from 'eslint-plugin-jsdoc'
+import * as path from 'path'
+import tsParser from '@typescript-eslint/parser'
+
+const plugins = {
+  '@typescript-eslint': tseslint.plugin,
+  'unused-imports': pluginUnusedImports,
+  import: pluginImport,
+  react: pluginReact,
+  'react-native': pluginReactNative,
+  node: pluginNode,
+  jsdoc: pluginJsdoc,
+}
+
+const rules = {
+  'unused-imports/no-unused-imports': 'warn',
+  '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  '@typescript-eslint/unbound-method': 'off',
+  'node/no-unsupported-features/es-syntax': 'off',
+  'node/no-missing-import': 'off',
+  'react-native/no-inline-styles': 'off',
+}
 
 export default [
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/build/**', 'eslint.config.js'],
+  },
+
   js.configs.recommended,
-  ...tseslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
 
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: 'single',
+    semi: false,
+    jsx: true,
+  }),
+
   {
-    files: ["**/*.ts", "**/*.tsx"],
-    globalIgnores: ["node_modules", "dist", "build"],
+    files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
+      parser: tsParser,
       parserOptions: {
-        project: ["./tsconfig.json"],
+        project: ['./tsconfig.json'],
         tsconfigRootDir: path.resolve(),
+        sourceType: 'module',
       },
     },
-    plugins: {
-      "unused-imports": require("eslint-plugin-unused-imports"),
-      import: require("eslint-plugin-import"),
-      react: require("eslint-plugin-react"),
-      "react-native": require("eslint-plugin-react-native"),
-      node: require("eslint-plugin-node"),
-      jsdoc: require("eslint-plugin-jsdoc"),
-    },
-    rules: {
-      "unused-imports/no-unused-imports": "warn",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_" },
-      ],
-      '@typescript-eslint/indent': ['error', 2],
-      "no-console": "warn",
-      indent: ['error', 2],
-      "node/no-unsupported-features/es-syntax": "off",
-      "node/no-missing-import": "off",
-      "react-native/no-inline-styles": "off",
+    plugins,
+    rules,
+  },
+  {
+    files: ['apps/frontend/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: ['./apps/frontend/tsconfig.json'],
+        tsconfigRootDir: path.resolve(),
+        sourceType: 'module',
+      },
     },
   },
-];
+  {
+    files: ['apps/backend/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: ['./apps/backend/tsconfig.json'],
+        tsconfigRootDir: path.resolve(),
+        sourceType: 'module',
+      },
+    },
+  },
+]
