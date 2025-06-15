f1468f87f1fc8ef6b92f667f49af40f963473823
Hanif Ulunnuha Hidayat <ulunnuha.h@gmail.com>
2025-06-15 08:49:48 +0700

    feat: add family module, service, and controller

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FamiliesModule } from './families/families.module';
 
 @Module({
-  imports: [],
+  imports: [FamiliesModule],
   controllers: [AppController],
   providers: [AppService],
 })
