2bd4a88e73377b73c69e55b403df3005d4825c11
Dimas Nabil Ahmad <dimnab@Dimnabs.local>
2025-06-27 10:28:20 +0700

    feat: setting up migration using datasource env

--- apps/backend/src/config/typeOrm.config.ts
+++ apps/backend/src/config/typeOrm.config.ts
@@ -1,22 +1,15 @@
 import { DataSource } from "typeorm";
+import * as dotenv from 'dotenv';
+
+dotenv.config()
 
 export default new DataSource({
-    // type: 'postgres',
-    // host: 'process.env.DB_HOST',
-    // port: Number(process.env.DB_PORT),
-    // username: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
-    // synchronize: false,
-    // entities: ['dist/**/entities/*.entity{.ts,.js}'],
-    // migrations: ['dist/src/migrations/*{.ts,.js}'],
-    // logging: true,
     type: 'postgres',
-    host: 'localhost',
-    port: 5433,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'pohon',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: false,
     entities: ['dist/**/entities/*.entity{.ts,.js}'],
     migrations: ['dist/src/migrations/*{.ts,.js}'],
