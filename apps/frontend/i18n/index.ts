002bc9a9bab06871a3704fb8c166beffd1b303b3
alvintriseptia <alvintryseptia@gmail.com>
2025-06-29 13:24:07 +0700

    chore(i18n): setup expo-localization

---
+++ apps/frontend/i18n/index.ts
@@ -0,0 +1,26 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import * as Localization from 'expo-localization'
+
+import en from './resources/en/translation.json'
+import id from './resources/id/translation.json'
+
+const resources = {
+  en: { translation: en },
+  id: { translation: id },
+}
+
+const locale = Localization.getLocales()[0]?.languageCode || 'en' // Fallback to English if no locale is found
+
+void i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: locale,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+
+export default i18n
