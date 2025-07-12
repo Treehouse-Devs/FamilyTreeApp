cf805c21c18b2185d1814c441e0fd4e6836a2200
Dimas Nabil <dimdimdimas@apps.ipb.ac.id>
2025-07-12 15:07:19 +0700

    feat: logger middleware for request and response

---
+++ apps/backend/src/logger/logger.middleware.ts
@@ -0,0 +1,22 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common'
+import { Response, Request } from 'express'
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP')
+  use(req: Request, res: Response, next: () => void) {
+    const startTime = Date.now()
+    const { ip, method, originalUrl } = req
+    const userAgent = req.get('user-agent') || ''
+
+    res.on('finish', () => {
+      const elapsedTime = Date.now() - startTime
+      const { statusCode } = res
+      const contentLength = res.get('content-length')
+
+      this.logger.log(`${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip} - ${elapsedTime}ms`)
+    })
+
+    next()
+  }
+}
