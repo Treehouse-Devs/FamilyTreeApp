883a415f29ef2dea2a4c2e2cf60c30b9bee00671
Dimas Nabil <dimdimdimas@apps.ipb.ac.id>
2025-07-12 18:57:14 +0700

    fix: eslint for user service

--- apps/backend/src/user/user.service.ts
+++ apps/backend/src/user/user.service.ts
@@ -1,16 +1,21 @@
 import {
+  ConflictException,
   Injectable,
   InternalServerErrorException,
   NotFoundException,
   UnauthorizedException,
-} from "@nestjs/common";
-import { RegisterUserDto } from "@myorg/dto/src/user/register-user.dto";
-import { LoginUserDto } from "@myorg/dto/src/user/login-user.dto";
-import { GoogleAuthDto } from "@myorg/dto/src/user/google-auth.dto";
-import { HttpRequestService } from "@app/http-request";
-import { FirebaseService } from './firebase.service';
-import { TokenService } from './token.service';
-import { UserFromToken } from "./user.types";
+} from '@nestjs/common'
+import { RegisterUserDto } from '@myorg/dto/src/user/register-user.dto'
+import { LoginUserDto } from '@myorg/dto/src/user/login-user.dto'
+import { LoginResponseDto } from '@myorg/dto/src/user/response/login-response.dto'
+import { GoogleAuthDto } from '@myorg/dto/src/user/google-auth.dto'
+import { HttpRequestService } from '@app/http-request'
+import { FirebaseService } from './firebase.service'
+import { TokenService } from './token.service'
+import { UserFromToken } from './user.types'
+import { FirebaseError } from 'firebase/app'
+import { UserRecord } from 'firebase-admin/lib/auth/user-record'
+import { AxiosResponse } from 'axios'
 
 @Injectable()
 export class UserService {
@@ -19,162 +24,186 @@ export class UserService {
     private firebaseService: FirebaseService,
     private tokenService: TokenService,
   ) {}
-  async signUp(registerUserDto: RegisterUserDto) {
-    const { name, email, password } = registerUserDto;
-    return this.firebaseService.createUser({ displayName: name, email, password });
+
+  async signUp(registerUserDto: RegisterUserDto): Promise<UserRecord> {
+    const { name, email, password } = registerUserDto
+    return await this.firebaseService.createUser({ displayName: name, email, password })
   }
 
-  async login(loginUserDto: LoginUserDto) {
-    const { email, password } = loginUserDto;
+  async login(loginUserDto: LoginUserDto): Promise<LoginResponseDto> {
+    const { email, password } = loginUserDto
 
     try {
-      await this.signInWithEmailAndPassword(email, password);
-      const userRecord = await this.firebaseService.getUserByEmail(email);
+      await this.signInWithEmailAndPassword(email, password)
+      const userRecord = await this.firebaseService.getUserByEmail(email)
       const tokenPayload: UserFromToken = {
         email: userRecord.email || '',
         uid: userRecord.uid,
-        displayName: userRecord.displayName
+        displayName: userRecord.displayName,
       }
 
-      const { accessToken, expiredAt, refreshToken, refreshTokenExpiredAt } = this.tokenService.createJwtTokens(tokenPayload);
+      const { accessToken, expiredAt, refreshToken, refreshTokenExpiredAt } = this.tokenService.createJwtTokens(tokenPayload)
       return {
         user: {
           uid: userRecord.uid,
-          email: userRecord.email,
-          displayName: userRecord.displayName,
+          email: userRecord.email || '',
+          displayName: userRecord.displayName || '',
           providerData: userRecord.providerData,
         },
         accessToken,
         refreshToken,
         refreshTokenExpiredAt,
         expiredAt,
-        message: "Login successful",
-      };
-    } catch (error: unknown) {
-      if (error.message && error.message.includes("INVALID_LOGIN_CREDENTIALS")) {
-        throw new UnauthorizedException("Invalid credentials.");
-      } else {
-        throw new InternalServerErrorException(error.message);
+        message: 'Login successful',
+      } as LoginResponseDto
+    }
+    catch (error: unknown) {
+      console.log('ERROR:', JSON.stringify(error))
+      if (error instanceof Error && error.message && error.message.includes('INVALID_LOGIN_CREDENTIALS')) {
+        throw new UnauthorizedException('Invalid credentials.')
+      }
+      else if (error instanceof Error) {
+        throw new InternalServerErrorException(error.message)
+      }
+      else {
+        throw new InternalServerErrorException('Unexpected error occured.')
       }
     }
   }
 
-  async googleAuth(googleAuthDto: GoogleAuthDto) {
-    const { idToken } = googleAuthDto;
+  async googleAuth(googleAuthDto: GoogleAuthDto): Promise<LoginResponseDto> {
+    const { idToken } = googleAuthDto
 
     try {
-      const decodedToken = await this.firebaseService.verifyIdToken(idToken);
+      const decodedToken = await this.firebaseService.verifyIdToken(idToken)
 
-      let userRecord;
+      let userRecord
       try {
         if (!decodedToken.email) {
-          throw new UnauthorizedException("Email not found in Google token");
+          throw new UnauthorizedException('Email not found in Google token')
         }
-        userRecord = await this.firebaseService.getUserByEmail(decodedToken.email);
-      } catch (error: unknown) {
-        if (error.code === 'auth/user-not-found') {
+        userRecord = await this.firebaseService.getUserByEmail(decodedToken.email)
+      }
+      catch (error: unknown) {
+        if (error instanceof FirebaseError && error.code === 'auth/user-not-found') {
           if (!decodedToken.email) {
-            throw new UnauthorizedException("Email not found in Google token");
+            throw new UnauthorizedException('Email not found in Google token')
           }
           userRecord = await this.firebaseService.createUser({
             email: decodedToken.email,
-            displayName: decodedToken.name,
-          });
-        } else {
-          throw error;
+            displayName: decodedToken.name as string,
+          })
+        }
+        else {
+          throw error
         }
       }
 
       const tokenPayload: UserFromToken = {
         email: userRecord.email || '',
         uid: userRecord.uid,
-        displayName: userRecord.displayName
+        displayName: userRecord.displayName,
       }
 
-      const { accessToken, expiredAt, refreshToken, refreshTokenExpiredAt } = this.tokenService.createJwtTokens(tokenPayload);
+      const { accessToken, expiredAt, refreshToken, refreshTokenExpiredAt } = this.tokenService.createJwtTokens(tokenPayload)
       return {
         user: {
           uid: userRecord.uid,
-          email: userRecord.email,
-          displayName: userRecord.displayName,
+          email: userRecord.email || '',
+          displayName: userRecord.displayName || '',
           providerData: userRecord.providerData,
         },
         accessToken,
         refreshToken,
         refreshTokenExpiredAt,
         expiredAt,
-        message: "Google authentication successful",
-      };
-    } catch (error: unknown) {
-      if (error.code === 'auth/invalid-id-token') {
-        throw new UnauthorizedException("Invalid Google ID token");
-      } else {
-        throw new InternalServerErrorException("Google authentication failed");
+        message: 'Google authentication successful',
+      } as LoginResponseDto
+    }
+    catch (error: unknown) {
+      if (error instanceof FirebaseError && error.code === 'auth/invalid-id-token') {
+        throw new UnauthorizedException('Invalid Google ID token')
+      }
+      else {
+        throw new InternalServerErrorException('Google authentication failed')
       }
     }
   }
 
-  private async signInWithEmailAndPassword(email: string, password: string) {
-    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.FB_API_KEY}`;
+  private async signInWithEmailAndPassword(email: string, password: string): Promise<AxiosResponse> {
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.FB_API_KEY}`
     try {
       return await this.httpRequestService.sendPostRequest(url, {
         email,
         password,
         returnSecureToken: true,
-      });
-    } catch (error: unknown) {
-      throw error?.data?.error;
+      })
     }
-  }
-
-  async resetPassword({ email, newPassword }: { email: string; newPassword: string }) {
-    try {
-      const userRecord = await this.firebaseService.getUserByEmail(email);
-      await this.firebaseService.updateUserPassword(userRecord.uid, newPassword);
-      return { message: "Password reset successful" };
-    } catch (error: unknown) {
-      if (error.code === "auth/user-not-found") {
-        throw new NotFoundException("User not found");
-      } else {
-        throw new InternalServerErrorException("Failed to reset password");
+    catch (error: unknown) {
+      if (typeof error === 'object' && error !== null && 'message' in error && error.message === 'INVALID_LOGIN_CREDENTIALS') {
+        throw new ConflictException('Invalid login credentials')
+      }
+      else if (error instanceof Error) {
+        throw new InternalServerErrorException(error.message)
+      }
+      else {
+        throw new InternalServerErrorException('Unexpected error occured.')
       }
     }
   }
 
-  async refreshToken(uid: string, refreshToken: string) {
-    const stored = this.tokenService.validateRefreshToken(uid, refreshToken);
-    const userRecord = await this.firebaseService.getUser(uid);
+  async resetPassword({ email, newPassword }: { email: string, newPassword: string }) {
+    try {
+      const userRecord = await this.firebaseService.getUserByEmail(email)
+      await this.firebaseService.updateUserPassword(userRecord.uid, newPassword)
+      return { message: 'Password reset successful' }
+    }
+    catch (error: unknown) {
+      if (error instanceof FirebaseError && error.code === 'auth/user-not-found') {
+        throw new NotFoundException('User not found')
+      }
+      else {
+        throw new InternalServerErrorException('Failed to reset password')
+      }
+    }
+  }
+
+  async refreshToken(uid: string, refreshToken: string): Promise<LoginResponseDto> {
+    const stored = this.tokenService.validateRefreshToken(uid, refreshToken)
+    const userRecord = await this.firebaseService.getUser(uid)
     const tokenPayload: UserFromToken = {
       email: userRecord.email || '',
       uid: userRecord.uid,
-      displayName: userRecord.displayName
+      displayName: userRecord.displayName,
     }
 
-    const { accessToken, expiredAt } = this.tokenService.createJwtTokens(tokenPayload);
+    const { accessToken, expiredAt } = this.tokenService.createJwtTokens(tokenPayload)
     return {
       accessToken,
       expiredAt,
       refreshTokenExpiredAt: stored.expiredAt,
       user: {
         uid: userRecord.uid,
-        email: userRecord.email,
-        displayName: userRecord.displayName,
+        email: userRecord.email || '',
+        displayName: userRecord.displayName || '',
         providerData: userRecord.providerData,
       },
       message: 'Token refreshed successfully',
-    };
+    } as LoginResponseDto
   }
 
   async deleteUser(uid: string) {
     try {
-      await this.firebaseService.deleteUser(uid);
-      this.tokenService.deleteRefreshToken(uid);
-      return { message: 'User deleted successfully' };
-    } catch (error: unknown) {
-      if (error.code === 'auth/user-not-found') {
-        throw new NotFoundException('User not found');
-      } else {
-        throw new InternalServerErrorException('Failed to delete user');
+      await this.firebaseService.deleteUser(uid)
+      this.tokenService.deleteRefreshToken(uid)
+      return { message: 'User deleted successfully' }
+    }
+    catch (error: unknown) {
+      if (error instanceof FirebaseError && error.code === 'auth/user-not-found') {
+        throw new NotFoundException('User not found')
+      }
+      else {
+        throw new InternalServerErrorException('Failed to delete user')
       }
     }
   }
