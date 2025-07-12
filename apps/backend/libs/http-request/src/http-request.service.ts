cc9486dc2bc9b4d60062046cd8059597926d7aa7
Dimas Nabil <dimdimdimas@apps.ipb.ac.id>
2025-07-12 18:57:14 +0700

    fix: eslint for user service

--- apps/backend/libs/http-request/src/http-request.service.ts
+++ apps/backend/libs/http-request/src/http-request.service.ts
@@ -1,17 +1,22 @@
-import { Injectable } from "@nestjs/common";
-import axios from "axios";
+import { Injectable } from '@nestjs/common'
+import axios, { AxiosError, AxiosResponse } from 'axios'
 
 @Injectable()
 export class HttpRequestService {
-  async sendPostRequest<T = unknown>(url: string, data: T) {
+  async sendPostRequest<T = unknown>(url: string, data: T): Promise<AxiosResponse> {
     try {
       const response = await axios.post(url, data, {
-        headers: { "Content-Type": "application/json" },
-      });
-      return response.data;
-    } catch (error: unknown) {
-      // console.log("error", error);
-      throw error.response;
+        headers: { 'Content-Type': 'application/json' },
+      })
+      return response
+    }
+    catch (error: unknown) {
+      if (error instanceof AxiosError) {
+        throw error.response?.data
+      }
+      else {
+        throw error
+      }
     }
   }
 }
