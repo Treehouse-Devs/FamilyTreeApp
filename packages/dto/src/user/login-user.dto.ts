4813b9ac5b2371c68dca983f245ce912e7479e08
Dimas Nabil <dimdimdimas@apps.ipb.ac.id>
2025-07-12 20:39:05 +0700

    wip: mailer service

--- packages/dto/src/user/login-user.dto.ts
+++ packages/dto/src/user/login-user.dto.ts
@@ -1,11 +1,34 @@
-import { IsEmail, IsNotEmpty, Length } from "class-validator";
+import { IsEmail, IsNotEmpty, Length } from 'class-validator'
 
 export class LoginUserDto {
   @IsNotEmpty()
   @IsEmail()
-  email!: string;
+  email!: string
 
   @IsNotEmpty()
   @Length(8, 20)
-  password!: string;
+  password!: string
+}
+
+export class LoginResponseDto {
+  user!: UserResponse
+  accessToken!: string
+  refreshToken?: string
+  refreshTokenExpiredAt!: number
+  expiredAt!: number
+  message!: string
+}
+
+export class UserInfo {
+  uid!: string
+  displayName!: string
+  email!: string
+  providerId!: string
+}
+
+export class UserResponse {
+  uid!: string
+  email!: string
+  displayName!: string
+  providerData!: UserInfo[]
 }
