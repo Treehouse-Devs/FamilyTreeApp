b74fedda8e28401e92a8ffbd2dffa69e7ffc418c
Dimas Nabil <dimdimdimas@apps.ipb.ac.id>
2025-07-12 15:07:19 +0700

    feat: logger middleware for request and response

--- apps/backend/src/user/get-user.decorator.ts
+++ apps/backend/src/user/get-user.decorator.ts
@@ -1,8 +1,8 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 
 export const GetUser = createParamDecorator(
   (data: string | undefined, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return data ? request.user?.[data] : request.user;
+    const request = ctx.switchToHttp().getRequest()
+    return data ? request.user?.[data] : request.user
   },
-); 
+)
--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -6,50 +6,50 @@ import {
   ValidationPipe,
   UseGuards,
   Delete,
-} from "@nestjs/common";
-import { UserService } from "./user.service";
-import { RegisterUserDto } from "@myorg/dto/src/user/register-user.dto";
-import { LoginUserDto } from "@myorg/dto/src/user/login-user.dto";
-import { GoogleAuthDto } from "@myorg/dto/src/user/google-auth.dto";
-import { ResetPasswordDto } from "@myorg/dto/src/user/reset-password.dto";
-import { JwtAuthGuard } from "./jwt-auth.guard";
-import { GetUser } from "./get-user.decorator";
-import { UserFromToken } from "./user.types";
+} from '@nestjs/common'
+import { UserService } from './user.service'
+import { RegisterUserDto } from '@myorg/dto/src/user/register-user.dto'
+import { LoginUserDto } from '@myorg/dto/src/user/login-user.dto'
+import { GoogleAuthDto } from '@myorg/dto/src/user/google-auth.dto'
+import { ResetPasswordDto } from '@myorg/dto/src/user/reset-password.dto'
+import { JwtAuthGuard } from './jwt-auth.guard'
+import { GetUser } from './get-user.decorator'
+import { UserFromToken } from './user.types'
 
-@Controller("user")
+@Controller('user')
 @UsePipes(new ValidationPipe({ transform: true }))
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post("/signup")
+  @Post('/signup')
   async signUp(@Body() registerUserDto: RegisterUserDto) {
-    return this.userService.signUp(registerUserDto);
+    return this.userService.signUp(registerUserDto)
   }
 
-  @Post("/login")
+  @Post('/login')
   async login(@Body() loginUserDto: LoginUserDto) {
-    return this.userService.login(loginUserDto);
+    return this.userService.login(loginUserDto)
   }
 
-  @Post("/google-auth")
+  @Post('/google-auth')
   async googleAuth(@Body() googleAuthDto: GoogleAuthDto) {
-    return this.userService.googleAuth(googleAuthDto);
+    return this.userService.googleAuth(googleAuthDto)
   }
 
-  @Post("/reset-password")
+  @Post('/reset-password')
   @UseGuards(JwtAuthGuard)
   async resetPassword(@Body() resetPasswordDto: ResetPasswordDto, @GetUser() user: UserFromToken) {
-    return this.userService.resetPassword({ ...resetPasswordDto, email: user.email });
+    return this.userService.resetPassword({ ...resetPasswordDto, email: user.email })
   }
 
-  @Post("/refresh-token")
-  async refreshToken(@Body() body: { uid: string; refreshToken: string }) {
-    return this.userService.refreshToken(body.uid, body.refreshToken);
+  @Post('/refresh-token')
+  async refreshToken(@Body() body: { uid: string, refreshToken: string }) {
+    return this.userService.refreshToken(body.uid, body.refreshToken)
   }
 
   @Delete()
   @UseGuards(JwtAuthGuard)
   async deleteUser(@GetUser() user: UserFromToken) {
-    return this.userService.deleteUser(user.uid);
+    return this.userService.deleteUser(user.uid)
   }
 }
