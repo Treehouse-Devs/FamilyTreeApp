e28205aab20b3f35ba891580fbbdc95101606431
Dimas Nabil <dimdimdimas@apps.ipb.ac.id>
2025-07-30 19:17:00 +0700

    feat: CRUD for family member

--- apps/backend/src/member/member.module.ts
+++ apps/backend/src/member/member.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { MemberController } from './member.controller';
-import { MemberService } from './member.service';
+import { Module } from '@nestjs/common'
+import { MemberController } from './member.controller'
+import { MemberService } from './member.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { FamilyMember } from './entities/family-member.entity'
+import { FamilyModule } from 'src/family/family.module'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([FamilyMember]), FamilyModule],
   controllers: [MemberController],
-  providers: [MemberService]
+  providers: [MemberService],
 })
 export class MemberModule {}
