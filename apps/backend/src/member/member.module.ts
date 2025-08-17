c8f6253bef51f8fbcc05ab3b7b967a376071dd1a
Dimas Nabil <dimdimdimas@apps.ipb.ac.id>
2025-08-17 09:08:31 +0700

    feat: add validation on create member

--- apps/backend/src/member/member.module.ts
+++ apps/backend/src/member/member.module.ts
@@ -4,9 +4,10 @@ import { MemberService } from './member.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { FamilyMember } from './entities/family-member.entity'
 import { FamilyModule } from 'src/family/family.module'
+import { FamilyRelationship } from './entities/family-relationship.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FamilyMember]), FamilyModule],
+  imports: [TypeOrmModule.forFeature([FamilyMember, FamilyRelationship]), FamilyModule],
   controllers: [MemberController],
   providers: [MemberService],
 })
