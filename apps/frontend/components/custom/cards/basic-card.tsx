a3f9486921182bcc62adaacb3ac875465b725d84
Ahmad Naufal <ahmedopank@gmail.com>
2026-02-22 13:26:13 +0700

    fix(main-page): add action sheet

--- apps/frontend/components/custom/cards/basic-card.tsx
+++ apps/frontend/components/custom/cards/basic-card.tsx
@@ -1,7 +1,8 @@
 import { cn } from '@gluestack-ui/utils/nativewind-utils'
-import { View, Pressable, StyleProp, ViewStyle } from 'react-native'
+import { View, StyleProp, ViewStyle } from 'react-native'
 import { Text } from '@/components/ui/text'
 import { VStack } from '@/components/ui/vstack'
+import { Pressable } from '@/components/ui/pressable'
 
 export const BasicCard = ({ children, className, onPress, category, style }: { children: React.ReactNode, className?: string, onPress?: () => void, category?: string, style?: StyleProp<ViewStyle> }) => {
   const cardContent = (
@@ -9,7 +10,7 @@ export const BasicCard = ({ children, className, onPress, category, style }: { c
       {!onPress && category && (
         <Text className="text-primary-700 text-sm mb-2 mr-auto ml-4">{category}</Text>
       )}
-      <View className={cn('w-full bg-secondary-0 data-[active=true]:bg-secondary-100 rounded-2xl border-2 border-secondary-500 py-2 items-center flex flex-column', className)}>
+      <View className={cn('w-full bg-secondary-0 group-active/card:bg-secondary-100 group-active/card:scale-95 rounded-2xl border-2 border-secondary-500 py-2 items-center flex flex-column', className)}>
         {children}
       </View>
     </VStack>
@@ -21,7 +22,7 @@ export const BasicCard = ({ children, className, onPress, category, style }: { c
         {category && (
           <Text className="text-primary-700 text-xs mb-2 mr-auto ml-4">{category}</Text>
         )}
-        <Pressable onPress={onPress}>
+        <Pressable onPress={onPress} className="group/card">
           {cardContent}
         </Pressable>
       </VStack>
